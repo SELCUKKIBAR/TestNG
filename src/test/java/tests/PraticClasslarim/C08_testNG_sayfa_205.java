3408062609655f1fb182cdf4d6fa1879078edef7
SELCUKKIBAR <selcukkibar88@gmail.com>
2023-05-25 18:08:20 +0300

    TestNG v2

--- src/test/java/tests/PraticClasslarim/C08_testNG_sayfa_205.java
+++ src/test/java/tests/PraticClasslarim/C08_testNG_sayfa_205.java
@@ -9,6 +9,7 @@ import org.testng.asserts.SoftAssert;
 import pages.ZeroWebPages;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,100 +18,76 @@ import java.util.List;
 public class C08_testNG_sayfa_205 {
 
     @Test
-    public void test01() throws InterruptedException {
-
-        //Yeni bir Class Olusturun : C03_SoftAssert
+    public void test01(){
         //1. “http://zero.webappsecurity.com/” Adresine gidin
         Driver.getDriver().get(ConfigReader.getProperty("zeroUrl"));
 
         //2. Sign in butonuna basin
-        ZeroWebPages zwb = new ZeroWebPages();
-        zwb.SignButon.click();
-
+        ZeroWebPages zeroPage = new ZeroWebPages();
+        zeroPage.SignButon.click();
 
         //3. Login kutusuna “username” yazin
-        zwb.userNameKutusu.sendKeys(ConfigReader.getProperty("zeruKullaniciAdi"));
+        zeroPage.userNameKutusu.sendKeys(ConfigReader.getProperty("zeruKullaniciAdi"));
 
         //4. Password kutusuna “password” yazin
-        zwb.passwordKutusu.sendKeys(ConfigReader.getProperty("zeroPassword"));
+        zeroPage.passwordKutusu.sendKeys(ConfigReader.getProperty("zeroPassword"));
 
+        ReusableMethods.bekle(3);
 
         //5. Sign in tusuna basin
-        zwb.girisYapButonu.click();
-
-        Driver.getDriver().navigate().back();
-
+        zeroPage.girisYapButonu.click();
 
         //6. Online banking menusu icinde Pay Bills sayfasina gidin
-        zwb.onlineBanking.click();
+        Driver.getDriver().navigate().back();
 
-        zwb.payBills.click();
+        zeroPage.onlineBanking.click();
 
+        zeroPage.payBills.click();
 
         //7. “Purchase Foreign Currency” tusuna basin
-        zwb.purchaseForeign.click();
-
+        zeroPage.purchaseForeign.click();
 
         //8. “Currency” drop down menusunden Eurozone’u secin
-        Select select = new Select(zwb.currencyDropDown);
-        select.selectByVisibleText(zwb.euroSec.getText());
-
+        Select select = new Select(zeroPage.currencyDropDown);
+        select.selectByVisibleText(zeroPage.euroSec.getText());
 
         //9. soft assert kullanarak "Eurozone (euro)" secildigini test edin
         SoftAssert softAssert = new SoftAssert();
-
-        softAssert.assertTrue(zwb.eurozoneGorunurlugu.isSelected(),"Eurozone Seçilmeli...");
-
+        softAssert.assertTrue(zeroPage.eurozoneGorunurlugu.isDisplayed());
 
         //10. soft assert kullanarak DropDown listesinin su secenekleri oldugunu test edin
-        //"Select One", "Australia (dollar)", "Canada (dollar)",
-        // "Switzerland (franc)","China (yuan)","Denmark (krone)",
-        // "Eurozone (euro)","Great Britain (pound)","Hong Kong (dollar)",
-        // "Japan (yen)","Mexico (peso)","Norway (krone)","New Zealand (dollar)",
-        // "Sweden (krona)","Singapore (dollar)","Thailand (baht)"
+        //"Select One", "Australia (dollar)", "Canada (dollar)","Switzerland (franc)","China
+        //(yuan)","Denmark (krone)","Eurozone (euro)","Great Britain (pound)","Hong Kong
+        // (dollar)","Japan (yen)","Mexico (peso)","Norway (krone)","New Zealand
+        // (dollar)","Sweden (krona)","Singapore (dollar)","Thailand (baht)"
 
-        List<WebElement> ddmListe = select.getOptions();
+         /*
+         String expectedOptions= "Select One\", \"Australia (dollar)\", \"Canada (dollar)\",\"Switzerland (franc)\",\"China\n" +
+                 "         //(yuan)\",\"Denmark (krone)\",\"Eurozone (euro)\",\"Great Britain (pound)\",\"Hong Kong\n" +
+                 "         (dollar)\",\"Japan (yen)\",\"Mexico (peso)\",\"Norway (krone)\",\"New Zealand\n" +
+                 "         (dollar)\",\"Sweden (krona)\",\"Singapore (dollar)\",\"Thailand (baht)";
 
+          */
 
-        List<String> expectedDRPDWNListe = new ArrayList<>();
-        expectedDRPDWNListe.add("Select One");
-        expectedDRPDWNListe.add("Australia (dollar)");
-        expectedDRPDWNListe.add("Canada (dollar)");
-        expectedDRPDWNListe.add("Switzerland (franc)");
-        expectedDRPDWNListe.add("China (yuan)");
-        expectedDRPDWNListe.add("Denmark (krone)");
-        expectedDRPDWNListe.add("Eurozone (euro)");
-        expectedDRPDWNListe.add("Great Britain (pound)");
-        expectedDRPDWNListe.add("Hong Kong (dollar)");
-        expectedDRPDWNListe.add("Japan (yen)");
-        expectedDRPDWNListe.add("Mexico (peso)");
-        expectedDRPDWNListe.add("Norway (krone)");
-        expectedDRPDWNListe.add("New Zealand (dollar)");
-        expectedDRPDWNListe.add("Sweden (krona)");
-        expectedDRPDWNListe.add("Singapore (dollar)");
-        expectedDRPDWNListe.add("Thailand (baht)");
+        String[] expectedOptions= {"Select One\\\", \\\"Australia (dollar)\\\", \\\"Canada (dollar)\\\",\\\"Switzerland (franc)\\\",\\\"China\\n\" +\n" +
+                "                 \"         //(yuan)\\\",\\\"Denmark (krone)\\\",\\\"Eurozone (euro)\\\",\\\"Great Britain (pound)\\\",\\\"Hong Kong\\n\" +\n" +
+                "                 \"         (dollar)\\\",\\\"Japan (yen)\\\",\\\"Mexico (peso)\\\",\\\"Norway (krone)\\\",\\\"New Zealand\\n\" +\n" +
+                "                 \"         (dollar)\\\",\\\"Sweden (krona)\\\",\\\"Singapore (dollar)\\\",\\\"Thailand (baht)"};
 
+        List<WebElement> actualOptions= select.getOptions();
 
-        List<String> actualDRPDWNListe = new ArrayList<>();
-
-        for (WebElement each:ddmListe
-             ) {
-            actualDRPDWNListe.add(each.getText());
+        for (WebElement eachOptions:actualOptions
+        ) {
+            System.out.println(eachOptions.getText());
         }
-        System.out.println(actualDRPDWNListe);
-        System.out.println("=============================");
-        System.out.println(expectedDRPDWNListe);
 
-        softAssert.assertEquals(actualDRPDWNListe,expectedDRPDWNListe);
+        softAssert.assertEquals(expectedOptions.length,actualOptions.size());
 
-        softAssert.assertAll();
+        for (int i = 0; i < expectedOptions.length; i++) {
+            softAssert.assertEquals(actualOptions.get(i).getText(),expectedOptions[i]);
+        }
 
-
-
-
-        // sayfayı kapat...
         Driver.closeDriver();
-
     }
 }
 
